abef21024cebe38fb11d72c7e1d74b2bcbf20e38
Ulyana Shabunina <66304635+UlyanaShabunina@users.noreply.github.com>
2022-02-26 14:07:26 +0300

    Update Program.cs

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,23 +1,59 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace Task_2
-{
-    static class Program
+static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        public static void FindMax(int[,] array,int m, int n, ref int max,ref int intLine, ref int intColumn)
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            int tempMax = 0;
+            int tempLine = 0, tempColumn = 0;
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j ++)
+                {
+                    if (array[i, j] > tempMax)
+                    {
+                        tempMax = array[i, j];
+                        tempLine = i;
+                        tempColumn = j;
+                    }
+                }
+            max = tempMax; intLine = tempLine; intColumn = tempColumn;
+        }
+        static void Main(string[] args)
+        {
+            int m = 2, n = 3;
+            int[,] intArray = new int[3, 2] { {6,3},{9,8},{5,10}};
+            int max = 0; int Line = 0; int Column = 0;
+            FindMax(intArray,3,2,ref max,ref Line,ref Column);
+ 
+            while (Line != 0)
+            {
+                int[] tempInt = new int[2];
+                for (int i = 0; i < 2; i++)
+                    tempInt[i] = intArray[Line - 1, i];
+                for (int i = 0; i < 2; i++)
+                    intArray[Line - 1, i] = intArray[Line,i];
+                for (int i = 0; i < 2; i++)
+                    intArray[Line, i] = tempInt[i];
+                Line--;
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                    Console.Write(intArray[i, j].ToString() + " ");
+                Console.WriteLine();
+            }
+            if (intArray[0, 0] != max)
+            {
+                int temp = intArray[0, 0];
+                intArray[0, 0] = intArray[0, Column];
+                intArray[0, Column] = temp;
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                    Console.Write(intArray[i, j].ToString() + " ");
+                Console.WriteLine();
+            }
+                //Console.WriteLine(max.ToString() + " " + Line.ToString() + " " + Column.ToString() + " ");
+                Console.ReadKey();
         }
     }
-}
